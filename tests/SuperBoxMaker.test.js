3ab610f478076d9721c909ac423642457b2d436b
Briar-Bear <ed.taylor04@yahoo.co.uk>
2023-12-14 15:47:08 +0000

    feature: added test for SuperBoxMaker

---
+++ tests/SuperBoxMaker.test.js
@@ -0,0 +1,8 @@
+import { expect, test } from '@jest/globals'
+import $e from '../lib/main.js'
+import { SuperBoxMaker } from '../lib/SuperBoxMaker.js'
+
+test('creating an instance of SuperBoxMaker', () => {
+  const box = $e('SuperBoxMaker', {})
+  expect(box.constructor).toBe(SuperBoxMaker)
+})
