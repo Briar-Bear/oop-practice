7221a4023e1b0f73eb7384c9cfa2712e5b50457b
Briar-Bear <ed.taylor04@yahoo.co.uk>
2023-12-14 15:47:20 +0000

    feature: added test for SuperContent

---
+++ tests/SuperContent.test.js
@@ -0,0 +1,8 @@
+import { expect, test } from '@jest/globals'
+import $e from '../lib/main.js'
+import { SuperContent } from '../lib/SuperContent.js'
+
+test('creating an instance of SuperContent', () => {
+  const content = $e('content', {})
+  expect(content.constructor).toBe(SuperContent)
+})
