aa417cf77d23a721677e0e1845b3df00bb615a3b
Briar-Bear <ed.taylor04@yahoo.co.uk>
2023-12-18 11:57:08 +0000

    Feature: making an image tester

--- tests/SuperImage.test.js
+++ tests/SuperImage.test.js
@@ -3,6 +3,17 @@ import $e from '../lib/main.js'
 import { SuperImage } from '../lib/SuperImage.js'
 
 test('creating an instance of SuperImage', () => {
-  const image = $e('image')
+  const image = $e('image', { src: 'imagePlaceHolder', height: 10, width: 10 })
   expect(image.constructor).toBe(SuperImage)
+})
+
+// test to check if adding an image works - DOESN'T WORK
+
+test('should add an image ', () => {
+  const image = $e('image', { src: 'imagePlaceHolder' })
+  const container = $e('content', { type: 'div' })
+
+  image.appendTo(container)
+
+  expect(image).toContain('imagePlaceHolder')
 })
