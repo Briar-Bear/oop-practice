a1facb9f6f44802178d283a1992eb9259da9ade2
Briar-Bear <ed.taylor04@yahoo.co.uk>
2023-12-18 14:57:28 +0000

    feature: added alignment test - WIP

--- tests/SuperDisplay.test.js
+++ tests/SuperDisplay.test.js
@@ -6,3 +6,14 @@ test('creating an instance of SuperDisplay', () => {
   const display = $e('display')
   expect(display.constructor).toBe(SuperDisplay)
 })
+
+// testing the alignment - UNDEFINED
+
+test('should center content', () => {
+  const content = $e('display', { position: 'centerBoth', direction: 'column' })
+  const button = $e('input', { type: 'button', name: 'test' })
+
+  button.appendTo(content)
+
+  expect(button.element.position).toBe('centerBoth')
+})
