3e31da60bab692702c20dcf8f0c294f7b52c6cf1
Briar-Bear <ed.taylor04@yahoo.co.uk>
2023-12-19 14:36:43 +0000

    Feature: added test for strike method - WIP

--- tests/SuperContent.test.js
+++ tests/SuperContent.test.js
@@ -6,3 +6,13 @@ test('creating an instance of SuperContent', () => {
   const content = $e('content')
   expect(content.constructor).toBe(SuperContent)
 })
+
+// testing the strike method - DOESNT WORK
+
+test('should strike out content', () => {
+  const content = $e('content', { type: 'h1', content: 'Test' })
+
+  content.strike()
+
+  expect(content.element).toBe('line-through')
+})
